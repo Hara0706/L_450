e9af08b47942ed3adbfcb849525f427e9f43c1c1
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-09-13 15:25:27 +0530

    Create q_18.cpp

---
+++ q_18.cpp
@@ -0,0 +1,28 @@
+// find pairs of sum equal to k
+// time and space is O(N)
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        
+        map<int, int> mp;
+        
+        for(int i=0; i<n; i++)
+        {
+            mp[arr[i]]++;
+        }
+        
+        int count = 0;
+        for(int i=0; i<n; i++)
+        {
+            count += mp[k-arr[i]];
+            
+            if(k - arr[i] == arr[i])
+            count--;
+        }
+        
+        return count/2;
+        
+      
+    }
+};
